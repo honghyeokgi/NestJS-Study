b7e4f6e4bcf90083c196dcf00ff199fd186be720
Honghyeokgi <gurrl377@naver.com>
2022-01-05 19:57:47 +0900

    nestJs 라우팅분리
    라우팅 분리와 싱글톤 패턴 적용

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,48 @@
 import * as express from "express";
+import catsRouter from "./cats/cats.routes";
 
 const app: express.Express = express();
-const port: number = 8000;
+const port = 8000;
 
-app.get("/test", (req: express.Request, res: express.Response) => {
-  console.log(req);
-  res.send({ name: "hhk", age: "21", friends: ["ss", "yy", "asd"] });
-});
+class Server {
+  public app: express.Application;
+  constructor() {
+    const app: express.Application = express();
+    this.app = app;
+  }
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  private setRoute() {
+    this.app.use(catsRouter);
+  }
 
-app.post("/test", (req: express.Request, res: express.Response) => {
-  res.send({ person: "hong" });
-});
+  private setMiddleware() {
+    // logging 미들웨어
+    this.app.use((req, res, next) => {
+      console.log(req.rawHeaders[1]);
+      console.log("this is logging middleware");
+      next();
+    });
+    // Json middleware
+    this.app.use(express.json());
+    this.setRoute();
+    // 404 error 미들웨어
+    this.app.use((req, res, next) => {
+      console.log("error middleware");
+      res.send({ error: "404 not found err" });
+    });
+  }
+
+  public listen() {
+    this.setMiddleware();
+    this.app.listen(port, () => {
+      console.log(`server is on..port:${port}`);
+    });
+  }
+}
+
+function init() {
+  const server = new Server();
+  server.listen;
+}
+
+init();
---
+++ src/cats/cats.service.ts
@@ -0,0 +1,129 @@
+import { Cat, CatType } from "./cats.model";
+import { Request, Response } from "express";
+
+// 고양이 전체 데이터 조회 READ
+export const readAllcat = (req: Request, res: Response) => {
+  try {
+    const cats = Cat;
+    //throw new Error("db connection error");
+    res.status(200).send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+// 특정 고양이 데이터 조회 READ
+export const readCat = (req: Request, res: Response) => {
+  try {
+    const params = req.params;
+    console.log(params);
+    const cat = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+    res.status(200).send({
+      sccess: true,
+      data: {
+        cat,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      sccess: false,
+      error: error.message,
+    });
+  }
+};
+
+// create 새로운 고양이 추가
+export const createCat = (req: Request, res: Response) => {
+  try {
+    const data = req.body;
+    console.log(data);
+    Cat.push(data);
+    res.status(200).send({
+      success: true,
+      data: { data },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+// update 고양이 데이터
+export const updateCat = (req: Request, res: Response) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+    let result;
+    Cat.forEach((cat) => {
+      if (cat.id === params.id) {
+        cat = body;
+        result = cat;
+      }
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat: result,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+// update 고양이 데이터 부분적 업데이트
+export const updatePartialCat = (req: Request, res: Response) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+    let result;
+    Cat.forEach((cat) => {
+      if (cat.id == params.id) {
+        cat = { ...cat, ...body };
+        result = cat;
+      }
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat: result,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+// delete 특정 아이디 고양이 삭제
+export const deleteCat = (req: Request, res: Response) => {
+  try {
+    const params = req.params;
+    const newCat = Cat.filter((cat) => cat.id !== params.id);
+    res.status(200).send({
+      success: true,
+      data: newCat,
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+};
